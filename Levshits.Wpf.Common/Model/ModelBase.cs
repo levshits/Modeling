74021253478f1cb52e423813c447af611979bd61
Levshits Valentin <levshits@yahoo.com>
2015-09-14 21:44:26 +0300

    Added common project

---
+++ Levshits.Wpf.Common/Model/ModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Levshits.Wpf.Common.Annotations;
+
+namespace Levshits.Wpf.Common.Model
+{
+    public class ModelBase:INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion INotifyPropertyChanged
+    }
+}
---
+++ Levshits.Wpf.Common/ViewModel/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Levshits.Wpf.Common.Annotations;
+
+namespace Levshits.Wpf.Common.ViewModel
+{
+    /// <summary>
+    /// Class ViewModelBase(Base class for all view model in wpf c# app).
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion INotifyPropertyChanged
+    }
+}
