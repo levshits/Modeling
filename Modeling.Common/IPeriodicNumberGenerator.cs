4c74793fd4f2d463a713f80bb2c36dd968fe7c06
Levshits Valentin <levshits@yahoo.com>
2015-09-14 21:45:31 +0300

    Added project with common for this project items

---
+++ Modeling.Common/IPeriodicNumberGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modeling.Common
+{
+    /// <summary>
+    /// Interface IPeriodicNumberGenerator
+    /// </summary>
+    public interface IPeriodicNumberGenerator: IRandomNumberGenerator
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is stop iteration if cycle detection enabled.
+        /// </summary>
+        /// <value><c>true</c> if this instance is stop on cycle detection enabled; otherwise, <c>false</c>.</value>
+        bool IsStopOnCycleDetectionEnabled { get; set; }
+
+        /// <summary>
+        /// Gets the period.
+        /// Return null if cycle not already detected
+        /// </summary>
+        int? GetPeriod();
+
+        /// <summary>
+        /// Gets the shift.
+        /// Return null if cycle not already detected
+        /// </summary>
+        int? GetShift();
+    }
+}
---
+++ Modeling.Common/IRandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Modeling.Common
+{
+    /// <summary>
+    /// Interface IRandomNumberGenerator
+    /// </summary>
+    public interface IRandomNumberGenerator: IEnumerator<double>
+    {
+        /// <summary>
+        /// Gets the generated values.
+        /// </summary>
+        List<double> GetGeneratedValues();
+    }
+}
