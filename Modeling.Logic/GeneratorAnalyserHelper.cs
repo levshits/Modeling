a7437d1d28e16c5656d10f34a26d24a70f9a0bea
Levshits Valentin <levshits@yahoo.com>
2015-09-14 21:45:57 +0300

    Added project with generator implementations

---
+++ Modeling.Logic/GeneratorAnalyserHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Modeling.Logic
+{
+    public class GeneratorAnalyserHelper
+    {
+        public static double GetMathExpected(List<double> values)
+        {
+            if (values.Count==0)
+            {
+                throw new ArgumentException();
+            }
+            return values.Sum()/values.Count;
+        }
+
+        public static double GetDispersion(List<double> values, double mx )
+        {
+            return GetMathExpected(values.Select(x => (x - mx)*(x- mx)).ToList());
+        }
+        public static double GetDispersion(List<double> values)
+        {
+            var mx = GetMathExpected(values);
+            return GetMathExpected(values.Select(x => (x - mx) * (x - mx)).ToList());
+        }
+
+        public static double GetStandartDeviation(double dx)
+        {
+            return Math.Sqrt(dx);
+        }
+
+        public static double GetStandartDeviation(List<double> values)
+        {
+            return GetStandartDeviation(GetDispersion(values));
+        }
+
+        public static double GetSequenceParameters(List<double> values)
+        {
+            var k = 0;
+            for (int i = 0; i < values.Count/2; i++)
+            {
+                if ((values[i]*values[i] + values[i + 1]*values[i + 1]) < 1)
+                {
+                    k++;
+                }
+            }
+        }
+    }
+}
---
+++ Modeling.Logic/LehmerRandomNumberGenerator.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Modeling.Common;
+
+namespace Modeling.Logic
+{
+    /// <summary>
+    /// Class LehmerRandomNumberGenerator.
+    /// Implement generator using Lehmer formula
+    /// </summary>
+    public class LehmerRandomNumberGenerator : IPeriodicNumberGenerator
+    {
+        #region fields
+
+        private readonly List<int> _generatedValues = new List<int>();
+        private int? _shift;
+        private int? _period;
+
+        #endregion fields
+
+        #region properties
+
+        public int M { get; set; }
+        public int A { get; set; }
+        public int R { get; set; }
+
+        #endregion properties
+
+        #region .ctor
+
+        public LehmerRandomNumberGenerator()
+        {
+        }
+
+        public LehmerRandomNumberGenerator(int a, int m, int r)
+        {
+            A = a;
+            M = m;
+            R = r;
+        }
+
+        #endregion .ctor
+
+        public bool MoveNext()
+        {
+            R = (A * R) % M;
+
+
+            if (_generatedValues.Any(x => x == R) && !_shift.HasValue)
+            {
+                _shift = _generatedValues.IndexOf(R) + 1;
+                _period = _generatedValues.Count - _shift;
+            }
+
+            var hasNext = !IsStopOnCycleDetectionEnabled || _generatedValues.All(x => x != R);
+            if (hasNext)
+            {
+                _generatedValues.Add(R);
+            }
+            return hasNext;
+        }
+
+        public void Reset()
+        {
+            _generatedValues.Clear();
+            _period = null;
+            _shift = null;
+        }
+
+        double IEnumerator<double>.Current => (double)Current;
+
+        public object Current => (double)_generatedValues.Last() / M;
+
+        public bool IsStopOnCycleDetectionEnabled { get; set; }
+
+        public int? GetPeriod()
+        {
+            return _period;
+        }
+
+        public int? GetShift()
+        {
+            return _shift;
+        }
+
+        public List<double> GetGeneratedValues()
+        {
+            return _generatedValues.Select(x => (double)x / M).ToList();
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+    }
+}
